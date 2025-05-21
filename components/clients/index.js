334d40e2740c331717d3099aebae7cea51282b68
Nazareno Gutierrez <nazarenojunin@gmail.com>
2025-05-21 20:55:06 -0300

    Optimized assets

--- components/clients/index.js
+++ components/clients/index.js
@@ -1,6 +1,6 @@
-export { default as cliente1 } from "./cliente1.jpeg";
-export { default as cliente3 } from "./cliente3.jpeg";
-export { default as cliente4 } from "./cliente4.jpeg";
-export { default as cliente5 } from "./cliente5.jpeg";
-export { default as cliente6 } from "./cliente6.jpeg";
-export { default as cliente7 } from "./cliente7.jpeg";
+export { default as cliente1 } from "./cliente1.webp";
+export { default as cliente3 } from "./cliente3.webp";
+export { default as cliente4 } from "./cliente4.webp";
+export { default as cliente5 } from "./cliente5.webp";
+export { default as cliente6 } from "./cliente6.webp";
+export { default as cliente7 } from "./cliente7.webp";
