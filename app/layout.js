de1e7895b0643734510678c4565a202d5cc60188
Nazareno Gutierrez <nazarenojunin@gmail.com>
2025-05-21 20:55:27 -0300

    New background

--- app/layout.js
+++ app/layout.js
@@ -17,7 +17,16 @@ export default function RootLayout({ children }) {
         <UserContextProvider>
           <DateContextProvider>
             <StepContextProvider>
-              <body className={`${fontPrincipal.className}`}>
+              <body className={`${fontPrincipal.className} relative`}>
+                <div className="absolute inset-0 pointer-events-none z-20 overflow-hidden min-h-screen">
+                  <div className="absolute top-0 left-0 w-[100%] h-px bg-gradient-to-r from-yellow-500/0 via-yellow-500/50 to-yellow-500/0 rotate-45 origin-top-left" />
+                  <div className="absolute top-1/2 left-0 w-[100%] h-px bg-gradient-to-r from-yellow-500/0 via-yellow-500/50 to-yellow-500/0 rotate-45 origin-top-left" />
+
+                  <div className="absolute left-1/4 top-0 h-[60vh] -rotate-45 w-px bg-yellow-500/10" />
+                  
+                  <div className="absolute right-1/4 top-0 h-[60vh] -rotate-45 w-px bg-yellow-500/10" />
+
+                </div>
                 <InitializeContexts />
                 <Header />
                 <FixedSocial />
